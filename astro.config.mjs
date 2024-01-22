16a16e4c0a301b9f6224c5e5eb2655d3135b41b2
estefang90 <36801483+estefang90@users.noreply.github.com>
2024-01-22 15:21:01 -0500

    lista primera parte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zireconsulting.com',
+  integrations: [tailwind(), mdx(), react(), vue(), svelte(), sitemap()]
+});
